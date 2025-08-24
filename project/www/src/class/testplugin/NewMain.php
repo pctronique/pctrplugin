3e9ad64ab6357b216686df0e832a677f9f8d1e7c
pctronique <pctronique@free.fr>
2025-08-24 06:43:18 +0200

    petite modif

---
+++ project/www/src/class/testplugin/NewMain.php
@@ -0,0 +1,19 @@
+<?php
+if (!class_exists('NewMain')) {
+
+    include_once __DIR__ . '/../pctrplugin/PctrPlugin.php';
+
+    class NewMain
+    {
+        public function __construct()
+        {
+            $pctrPlugin = new PctrPlugin("AddPluginInterface");
+            $pctrPlugin->loadPlugins();
+            foreach ($pctrPlugin->getPlugins() as $value) {
+                echo $value->getName() . " : " . $value->getName() . "<br />";
+            }
+        }
+        
+    }
+
+}
